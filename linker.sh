4cd1cfb041184f3a26238c8eebeebf0d9e9c4c4a
haconeco <admin@haccat.com>
2014-05-11 18:07:03 +0900

    added linker.sh

---
+++ linker.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+ln -si ~/dotfiles/_zshrc ~/.zshrc
+ln -si ~/dotfiles/_vim ~/.vim
+ln -si ~/dotfiles/_vimrc ~/.vimrc
+ln -si ~/dotfiles/_tmux_conf ~/.tmux.conf
+ln -si ~/dotfiles/_ctags ~/.ctags
