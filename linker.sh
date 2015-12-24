89666abeca0d9ed818341844357c8dac59bcf2cd
haconeco <admin@haccat.com>
2015-12-24 16:13:55 +0900

    gvimrc

--- linker.sh
+++ linker.sh
@@ -9,3 +9,4 @@ ln -si ~/dotfiles/.Rprofile ~/.Rprofile
 ln -si ~/dotfiles/.latexmkrc ~/.latexmkrc
 ln -si ~/dotfiles/.gitignore_global ~/.gitignore_global
 ln -si ~/dotfiles/.config ~/.config
+ln -si ~/dotfiles/.gvimrc ~/.gvimrc
